efec4e9f248a87603ad45bf497ddfe7b7288dabf
Mateusz Najdek <mateusz14321@gmail.com>
2022-04-04 14:35:46 +0200

    Refactor of message Converter

--- src/main/java/pl/edu/agh/hiputs/communication/utils/MessageConverter.java
+++ src/main/java/pl/edu/agh/hiputs/communication/utils/MessageConverter.java
@@ -7,31 +7,18 @@ import java.io.*;
 public class MessageConverter {
 
     public static byte[] toByteArray(Message message) throws IOException {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        byte[] encodedMsg;
-        try {
-            ObjectOutputStream out = new ObjectOutputStream(bos);
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bos)) {
             out.writeObject(message);
             out.flush();
-            encodedMsg = bos.toByteArray();
-        } finally {
-            bos.close();
+            return bos.toByteArray();
         }
-
-        return encodedMsg;
     }
 
     public static Message toMessage(byte[] encodedMessage) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream bos = new ByteArrayInputStream(encodedMessage);
-        Message message;
-
-        try {
-            ObjectInputStream in = new ObjectInputStream(bos);
-            message = (Message) in.readObject();
-        } finally {
-            bos.close();
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(encodedMessage);
+             ObjectInputStream in = new ObjectInputStream(bis)) {
+            return (Message) in.readObject();
         }
-
-        return message;
     }
 }
