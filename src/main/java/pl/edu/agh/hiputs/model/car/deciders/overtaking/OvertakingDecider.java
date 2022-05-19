42b9b99ccc47f55d92bbd5877cd332508a1826bf
Damian741 <ddymek25@wp.pl>
2022-05-19 11:20:38 +0200

    Refactor information gathering algorithm for overtaking

--- src/main/java/pl/edu/agh/hiputs/model/car/deciders/overtaking/OvertakingDecider.java
+++ src/main/java/pl/edu/agh/hiputs/model/car/deciders/overtaking/OvertakingDecider.java
@@ -8,6 +8,7 @@ import pl.edu.agh.hiputs.model.id.JunctionId;
 import pl.edu.agh.hiputs.model.id.LaneId;
 import pl.edu.agh.hiputs.model.map.mapfragment.RoadStructureReader;
 import pl.edu.agh.hiputs.model.map.roadstructure.HorizontalSign;
+import pl.edu.agh.hiputs.model.map.roadstructure.Lane;
 import pl.edu.agh.hiputs.model.map.roadstructure.LaneReadable;
 
 public class OvertakingDecider {
@@ -29,10 +30,7 @@ public class OvertakingDecider {
   public Optional<OvertakingEnvironment> getOvertakingInformation(CarEditable currentCar, CarEnvironment carEnvironment,
       RoadStructureReader roadStructureReader) {
     LaneReadable currentLane = roadStructureReader.getLaneReadable(currentCar.getLaneId());
-    if (currentLane.getLeftNeighbor().isEmpty() || currentLane.getLeftNeighbor()
-        .get()
-        .getHorizontalSign()
-        .equals(HorizontalSign.OPPOSITE_DIRECTION_SOLID_LINE)) {
+    if (!canOvertakeOnLane(currentLane)) {
       return Optional.empty(); // we can't overtake
     }
     JunctionId nextJunctionId = currentLane.getOutgoingJunctionId();
@@ -41,51 +39,75 @@ public class OvertakingDecider {
     Optional<CarReadable> carBeforeOvertakenCar = currentLane.getCarInFrontReadable(overtakenCar); // find C car
     Optional<CarReadable> oppositeCar =
         oppositeLane.getCarBeforePosition(oppositeLane.getLength() - currentCar.getPositionOnLane()); // find D car
-    double distanceBeforeOvertakenCar;
-    double distanceOnOppositeLane;
-    if (nextJunctionId.isCrossroad() || (carBeforeOvertakenCar.isPresent()
-        && oppositeCar.isPresent())) { // for future if we find oppositeCar car, we need to find carBeforeOvertakenCar?
-      distanceBeforeOvertakenCar =
+    OvertakingEnvironment overtakingEnvironment;
+    if (foundAllInformation(nextJunctionId, carBeforeOvertakenCar,
+        oppositeCar)) { // for future if we find oppositeCar car, we need to find carBeforeOvertakenCar?
+      double distanceBeforeOvertakenCar =
           carBeforeOvertakenCar.map(car -> car.getPositionOnLane() - car.getLength()).orElse(currentLane.getLength())
               - overtakenCar.getPositionOnLane();
-      distanceOnOppositeLane = currentLane.getLength() - oppositeCar.map(car -> car.getPositionOnLane()).orElse(0.0)
-          - currentCar.getPositionOnLane();
+      double distanceOnOppositeLane =
+          currentLane.getLength() - oppositeCar.map(car -> car.getPositionOnLane()).orElse(0.0)
+              - currentCar.getPositionOnLane();
+      overtakingEnvironment = new OvertakingEnvironment(oppositeCar, carBeforeOvertakenCar, distanceOnOppositeLane,
+          distanceBeforeOvertakenCar);
     } else {
-      distanceBeforeOvertakenCar = 0;
-      distanceOnOppositeLane = 0;
-      int offset = 0;
-      Optional<LaneId> nextLaneId;
-      LaneReadable nextLane;
-      LaneReadable nextOppositeLane;
-      while (!nextJunctionId.isCrossroad() && !(carBeforeOvertakenCar.isPresent() && oppositeCar.isPresent())) {
-        nextLaneId = currentCar.getRouteWithLocation().getOffsetLaneId(offset++);
-        if (nextLaneId.isEmpty()) {
-          break;
-        }
-        nextLane = roadStructureReader.getLaneReadable(nextLaneId.get());
-        if (nextLane.getLeftNeighbor().isEmpty() || nextLane.getLeftNeighbor()
-            .get()
-            .getHorizontalSign()
-            .equals(HorizontalSign.OPPOSITE_DIRECTION_SOLID_LINE)) {
-          break; // we can't overtake any further
-        }
-        nextOppositeLane = roadStructureReader.getLaneReadable(nextLane.getLeftNeighbor().get().getLaneId());
-        distanceBeforeOvertakenCar += currentLane.getLength();
-        distanceOnOppositeLane += oppositeLane.getLength();
-        nextJunctionId = nextLane.getOutgoingJunctionId();
-        carBeforeOvertakenCar = nextLane.getCarAtEntryReadable();
-        oppositeCar = nextOppositeLane.getCarAtExitReadable();
-        currentLane = nextLane;
-        oppositeLane = nextOppositeLane;
-      }
-      distanceBeforeOvertakenCar +=
-          carBeforeOvertakenCar.map(car -> car.getPositionOnLane() - car.getLength()).orElse(currentLane.getLength())
-              - overtakenCar.getPositionOnLane();
-      distanceOnOppositeLane += oppositeLane.getLength() - oppositeCar.map(car -> car.getPositionOnLane()).orElse(0.0)
-          - currentCar.getPositionOnLane();
+      overtakingEnvironment =
+          searchRouteForOvertakingInformation(currentCar, overtakenCar, carBeforeOvertakenCar, oppositeCar, nextJunctionId,
+              currentLane, oppositeLane, roadStructureReader);
     }
-    return Optional.of(new OvertakingEnvironment(oppositeCar, carBeforeOvertakenCar, distanceOnOppositeLane,
-        distanceBeforeOvertakenCar));
+    return Optional.of(overtakingEnvironment);
+  }
+
+  /**
+   * Iterate over Car route up to the closest crossroad or until all information are found
+   * @return
+   */
+  private OvertakingEnvironment searchRouteForOvertakingInformation(CarEditable currentCar, CarReadable overtakenCar,
+      Optional<CarReadable> carBeforeOvertakenCar, Optional<CarReadable> oppositeCar, JunctionId nextJunctionId,
+      LaneReadable currentLane, LaneReadable oppositeLane, RoadStructureReader roadStructureReader) {
+    double distanceBeforeOvertakenCar = 0;
+    double distanceOnOppositeLane = 0;
+    int offset = 0;
+    Optional<LaneId> nextLaneId;
+    LaneReadable nextLane;
+    LaneReadable nextOppositeLane;
+    while (!foundAllInformation(nextJunctionId, carBeforeOvertakenCar, oppositeCar)) {
+      nextLaneId = currentCar.getRouteWithLocation().getOffsetLaneId(++offset);
+      if (nextLaneId.isEmpty()) {
+        break;
+      }
+      nextLane = roadStructureReader.getLaneReadable(nextLaneId.get());
+      if (!canOvertakeOnLane(nextLane)) {
+        break; // we can't overtake any further
+      }
+      nextOppositeLane = roadStructureReader.getLaneReadable(nextLane.getLeftNeighbor().get().getLaneId());
+      distanceBeforeOvertakenCar += currentLane.getLength();
+      distanceOnOppositeLane += oppositeLane.getLength();
+      nextJunctionId = nextLane.getOutgoingJunctionId();
+      carBeforeOvertakenCar = nextLane.getCarAtEntryReadable();
+      oppositeCar = nextOppositeLane.getCarAtExitReadable();
+      currentLane = nextLane;
+      oppositeLane = nextOppositeLane;
+    }
+    distanceBeforeOvertakenCar +=
+        carBeforeOvertakenCar.map(car -> car.getPositionOnLane() - car.getLength()).orElse(currentLane.getLength())
+            - overtakenCar.getPositionOnLane();
+    distanceOnOppositeLane += oppositeLane.getLength() - oppositeCar.map(car -> car.getPositionOnLane()).orElse(0.0)
+        - currentCar.getPositionOnLane();
+    return new OvertakingEnvironment(oppositeCar, carBeforeOvertakenCar, distanceOnOppositeLane,
+        distanceBeforeOvertakenCar);
+  }
+
+  private boolean canOvertakeOnLane(LaneReadable lane) {
+    return lane.getLeftNeighbor().isPresent() && lane.getLeftNeighbor()
+        .get()
+        .getHorizontalSign()
+        .equals(HorizontalSign.OPPOSITE_DIRECTION_DOTTED_LINE);
+  }
+
+  private boolean foundAllInformation(JunctionId nextJunctionId, Optional<CarReadable> carBeforeOvertakenCar,
+      Optional<CarReadable> oppositeCar) {
+    return nextJunctionId.isCrossroad() || (carBeforeOvertakenCar.isPresent() && oppositeCar.isPresent());
   }
 
 }
