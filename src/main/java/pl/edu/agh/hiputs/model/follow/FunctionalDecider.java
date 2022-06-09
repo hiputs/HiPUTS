d353d0697cf8ebaa38005310f5626d35efeba53f
Sebastian <wilk.s98@wp.pl>
2022-06-09 08:44:33 +0200

    JavaDoc for Decider explanation

--- src/main/java/pl/edu/agh/hiputs/model/follow/FunctionalDecider.java
+++ src/main/java/pl/edu/agh/hiputs/model/follow/FunctionalDecider.java
@@ -4,6 +4,10 @@ import pl.edu.agh.hiputs.model.car.CarEnvironment;
 import pl.edu.agh.hiputs.model.car.CarReadable;
 import pl.edu.agh.hiputs.model.map.mapfragment.RoadStructureReader;
 
+/**
+ * More information in MainDecider file
+ */
+
 public interface FunctionalDecider {
 
   double makeDecision(CarReadable managedCar, CarEnvironment environment, RoadStructureReader roadStructureReader);
