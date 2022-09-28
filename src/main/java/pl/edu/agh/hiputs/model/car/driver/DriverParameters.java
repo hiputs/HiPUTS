c13664ac80b8881efa3af8c552342535e8d45c1c
Sebastian <wilk.s98@wp.pl>
2022-09-28 09:18:14 +0200

    Repair acceleration parameters

--- src/main/java/pl/edu/agh/hiputs/model/car/driver/DriverParameters.java
+++ src/main/java/pl/edu/agh/hiputs/model/car/driver/DriverParameters.java
@@ -6,14 +6,14 @@ import lombok.Value;
 public class DriverParameters {
   private final double IdmDistanceHeadway = 2.0;
   private final double IdmTimeHeadway = 2.0;
-  private final double IdmNormalAcceleration = 3.5;
-  private final double IdmNormalDeceleration = 2.0;
+  private final double IdmNormalAcceleration = 2.0;
+  private final double IdmNormalDeceleration = 3.5;
   private final int IdmDelta = 4;
 
   private final double TrailTimeDelta = 1.25;
   private final double TrailConflictAreaLength = 5.0;
 
-  private final double DriverTimeStep = 0.5;
+  private final double DriverTimeStep = 1;
   private final int GiveWayWaitTime = 5;
   private final int MovePermanentWaitTime = 40;
 }
