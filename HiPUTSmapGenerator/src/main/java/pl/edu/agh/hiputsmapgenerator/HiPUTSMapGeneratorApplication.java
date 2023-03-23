5b1b4ae64b4e9e6c7acdfa0c57c9565a755628d9
Natalia <natib13@gmail.com>
2023-03-23 11:45:30 +0100

    map grows horizontally and vertically

--- HiPUTSmapGenerator/src/main/java/pl/edu/agh/hiputsmapgenerator/HiPUTSMapGeneratorApplication.java
+++ HiPUTSmapGenerator/src/main/java/pl/edu/agh/hiputsmapgenerator/HiPUTSMapGeneratorApplication.java
@@ -7,37 +7,37 @@ import pl.edu.agh.hiputsmapgenerator.model.Lane;
 import pl.edu.agh.hiputsmapgenerator.model.Node;
 import pl.edu.agh.hiputsmapgenerator.model.Patch;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
+
+import static java.lang.Math.sqrt;
 
 
 public class HiPUTSMapGeneratorApplication {
 
     private static final String MAP_PATH = "..\\data\\";
 
-    private static final Dims mapDims = new Dims(9,9); // all nodes in map
+    private static final Pair mapDims = new Pair(12,15); // all nodes in map for one worker
 
-    private static Dims patchDims = new Dims(3,3); // nodes in patch
+    private static Pair patchDims = new Pair(3,3); // nodes in patch
     private static int laneLength = 500; //meters
 
 
-    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+    public static void main(String[] args) throws IOException {
 
         if(args.length > 3) {
             laneLength = Integer.parseInt(args[1]);
-            patchDims = new Dims(Integer.parseInt(args[2]),Integer.parseInt(args[3]));
+            patchDims = new Pair(Integer.parseInt(args[2]),Integer.parseInt(args[3]));
         }
 
-        mapDims.setDimX(patchDims.getDimX() * (int) mapDims.getDimX() / patchDims.getDimX());
-        mapDims.setDimY(patchDims.getDimY() * (int) mapDims.getDimY() / patchDims.getDimY());
+        mapDims.setX(patchDims.getX() * (int) mapDims.getX() / patchDims.getX());
+        mapDims.setY(patchDims.getY() * (int) mapDims.getY() / patchDims.getY());
 
-        genMap(1);
-       genMap(2);
-//        genMap(4);
+        // genMap(1);
+        // genMap(2);
+        genMap(4);
         // genMap(8);
         // genMap(12);
         // genMap(16);
@@ -49,10 +49,11 @@ public class HiPUTSMapGeneratorApplication {
 
     }
 
-    private static void genMap(int workerCount) throws FileNotFoundException, UnsupportedEncodingException {
-        Dims totalSize = new Dims(mapDims.getDimX() * workerCount, mapDims.getDimY());
+    private static void genMap(int workerCount) throws IOException {
+        Pair multi = calc_dims_multipliers(workerCount);
+        Pair totalSize = new Pair(mapDims.getX() * multi.getX(), mapDims.getY()*multi.getY());
 
-        Node[][] map = new Node[totalSize.getDimX()][totalSize.getDimY()];
+        Node[][] map = new Node[totalSize.getX()][totalSize.getY()];
 
         fillMap(map, totalSize);
 
@@ -71,15 +72,31 @@ public class HiPUTSMapGeneratorApplication {
         savePatches(patches, workerCount);
     }
 
-    private static void savePatches(List<Patch> patches, int workerCount) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter(MAP_PATH + "square-map" + workerCount + "/patches.csv", "UTF-8");
+    private static Pair calc_dims_multipliers(int workerCount){
+        int square = (int) sqrt(workerCount);
+        int multiX = 1;
+
+
+        if(workerCount / square == (int) workerCount / square) {
+            multiX = square;
+        }
+        else if(workerCount / (square+1) == (int) workerCount / (square+1)){
+            multiX = square+1;
+        }
+
+
+        return new Pair(multiX, workerCount / multiX);
+    }
+
+    private static void savePatches(List<Patch> patches, int workerCount) throws IOException {
+        PrintWriter writer = new PrintWriter(MAP_PATH + "square-map" + workerCount + "/patches.csv", StandardCharsets.UTF_8);
         writer.println("id,neighbouring_patches_ids");
         patches.forEach(p -> writer.println(p.toString()));
         writer.close();
     }
 
-    private static void saveNodes(Node[][] map, int workerCount) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter(MAP_PATH + "square-map" + workerCount + "/nodes.csv", "UTF-8");
+    private static void saveNodes(Node[][] map, int workerCount) throws IOException {
+        PrintWriter writer = new PrintWriter(MAP_PATH + "square-map" + workerCount + "/nodes.csv", StandardCharsets.UTF_8);
         writer.println("id,longitude,latitude,is_crossroad,patch_id,tags");
 
         for (int i = 0; i < map.length; i++) {
@@ -91,17 +108,17 @@ public class HiPUTSMapGeneratorApplication {
         writer.close();
     }
 
-    private static void saveEdges(List<Lane> edges, int workerCount) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter(MAP_PATH + "square-map" + workerCount + "/edges.csv", "UTF-8");
+    private static void saveEdges(List<Lane> edges, int workerCount) throws IOException {
+        PrintWriter writer = new PrintWriter(MAP_PATH + "square-map" + workerCount + "/edges.csv", StandardCharsets.UTF_8);
         writer.println("source,target,length,max_speed,is_priority_road,is_one_way,patch_id,tags");
         edges.forEach(e -> writer.println(e.toString()));
         writer.close();
     }
 
-    private static void createPatches(Node[][] map, Dims dims, List<Patch> patches) {
+    private static void createPatches(Node[][] map, Pair dims, List<Patch> patches) {
 //        int n = size / 2;
-        int xPatches = dims.getDimX()/patchDims.getDimX();
-        int yPatches = dims.getDimY()/ patchDims.getDimY();
+        int xPatches = dims.getX()/patchDims.getX();
+        int yPatches = dims.getY()/ patchDims.getY();
 
         for (int i = 0; i < xPatches; i++) {
             for (int j = 0; j < yPatches; j++) {
@@ -133,11 +150,11 @@ public class HiPUTSMapGeneratorApplication {
 
                 List<Lane> allPatchLanes = new LinkedList<>();
 
-                int dimX = patchDims.getDimX();
-                int dimY = patchDims.getDimY();
+                int dimX = patchDims.getX();
+                int dimY = patchDims.getY();
 
-                for(int k=0;k<patchDims.getDimX();k++) {
-                    for (int l = 0; l < patchDims.getDimY(); l++) {
+                for(int k = 0; k<patchDims.getX(); k++) {
+                    for (int l = 0; l < patchDims.getY(); l++) {
                         map[i * dimX + k][j * dimY + l].setPatch_id(patch.getId());
                         allPatchLanes.addAll(map[i * dimX + k][j * dimY + l].getLanes());
                     }
@@ -148,9 +165,9 @@ public class HiPUTSMapGeneratorApplication {
         }
     }
 
-    private static void createLanesVertical(Node[][] map, Dims dims, List<Lane> edges) {
-        for (int i = 0; i < dims.getDimY(); i++) {
-            for (int j = 0; j < dims.getDimX() -1; j++) {
+    private static void createLanesVertical(Node[][] map, Pair dims, List<Lane> edges) {
+        for (int i = 0; i < dims.getY(); i++) {
+            for (int j = 0; j < dims.getX() -1; j++) {
                 Lane lane1 = new Lane(map[j][i].getId(), map[j+1][i].getId(), laneLength);
                 Lane lane2 = new Lane(map[j+1][i].getId(), map[j][i].getId(), laneLength);
                 map[j][i].getLanes().add(lane1);
@@ -162,10 +179,10 @@ public class HiPUTSMapGeneratorApplication {
         }
     }
 
-    private static void createTorusLanesVertical(Node[][] map, Dims dims, List<Lane> edges) {
-        int lastIdx = dims.getDimY() - 1;
+    private static void createTorusLanesVertical(Node[][] map, Pair dims, List<Lane> edges) {
+        int lastIdx = dims.getY() - 1;
 
-        for (int j = 0; j < dims.getDimX(); j++) {
+        for (int j = 0; j < dims.getX(); j++) {
             Lane lane1 = new Lane(map[j][0].getId(), map[j][lastIdx].getId(), laneLength);
             Lane lane2 = new Lane(map[j][lastIdx].getId(), map[j][0].getId(), laneLength);
             map[j][0].getLanes().add(lane1);
@@ -177,9 +194,9 @@ public class HiPUTSMapGeneratorApplication {
 
     }
 
-    private static void createLanesHorizontal(Node[][] map, Dims dims, List<Lane> edges) {
-        for (int i = 0; i < dims.getDimX(); i++) {
-            for (int j = 0; j < dims.getDimY() - 1; j++) {
+    private static void createLanesHorizontal(Node[][] map, Pair dims, List<Lane> edges) {
+        for (int i = 0; i < dims.getX(); i++) {
+            for (int j = 0; j < dims.getY() - 1; j++) {
                 Lane lane1 = new Lane(map[i][j].getId(), map[i][j + 1].getId(), laneLength);
                 Lane lane2 = new Lane(map[i][j + 1].getId(), map[i][j].getId(), laneLength);
 
@@ -192,10 +209,10 @@ public class HiPUTSMapGeneratorApplication {
         }
     }
 
-    private static void createTorusLanesHorizontal(Node[][] map, Dims dims, List<Lane> edges) {
-        int lastIdx = dims.getDimX() - 1;
+    private static void createTorusLanesHorizontal(Node[][] map, Pair dims, List<Lane> edges) {
+        int lastIdx = dims.getX() - 1;
 
-        for (int j = 0; j < dims.getDimY(); j++) {
+        for (int j = 0; j < dims.getY(); j++) {
             Lane lane1 = new Lane(map[0][j].getId(), map[lastIdx][j].getId(), laneLength);
             Lane lane2 = new Lane(map[lastIdx][j].getId(), map[0][j].getId(), laneLength);
 
@@ -207,10 +224,10 @@ public class HiPUTSMapGeneratorApplication {
         }
     }
 
-    private static void fillMap(Node[][] map, Dims dims) {
+    private static void fillMap(Node[][] map, Pair dims) {
         float multiplier = (float) laneLength / 100;
-        for (int i = 0; i < dims.getDimX(); i++) {
-            for (int j = 0; j < dims.getDimY(); j++) {
+        for (int i = 0; i < dims.getX(); i++) {
+            for (int j = 0; j < dims.getY(); j++) {
                 map[i][j] = Node.builder()
                         .id(String.format("N%d-%d", i, j))
                         .latitude(String.valueOf(j*multiplier))
@@ -223,9 +240,9 @@ public class HiPUTSMapGeneratorApplication {
     @Getter
     @Setter
     @AllArgsConstructor
-    private static class Dims {
-        private int dimX; // longitude
-        private int dimY; // latitude
+    private static class Pair {
+        private int x; // in dims: longitude
+        private int y; // in dims: latitude
 
     }
 
