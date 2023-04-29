0ff78dcbfc132eafbb69fbc824cd8bb6a29f01b3
Łukasz Wolski <wolski0420@gmail.com>
2023-04-29 13:26:56 +0200

    Assigned traffic indicators to roads

---
+++ src/main/java/pl/edu/agh/hiputs/partition/mapper/util/indicator/TIOnCrossroadProcessor.java
@@ -0,0 +1,27 @@
+package pl.edu.agh.hiputs.partition.mapper.util.indicator;
+
+import java.util.Optional;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+import pl.edu.agh.hiputs.partition.model.JunctionData;
+import pl.edu.agh.hiputs.partition.model.WayData;
+import pl.edu.agh.hiputs.partition.model.graph.Node;
+import pl.edu.agh.hiputs.partition.model.lights.indicator.TrafficIndicator;
+
+@Service
+@Order(1)
+public class TIOnCrossroadProcessor implements TIProcessor, TIAllocator{
+
+  @Override
+  public void checkAndAllocate(Node<JunctionData, WayData> node) {
+    if (node.getData().isCrossroad()) {
+      allocateAroundNode(node);
+    }
+  }
+
+  @Override
+  public void allocateAroundNode(Node<JunctionData, WayData> node) {
+    node.getIncomingEdges().forEach(incomingEdge ->
+        incomingEdge.getData().setTrafficIndicator(Optional.of(new TrafficIndicator())));
+  }
+}
