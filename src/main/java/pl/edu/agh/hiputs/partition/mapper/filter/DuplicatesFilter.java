756797aa8fcbedfb59260c05a0b28424ae0a9c03
Łukasz Wolski <wolski0420@gmail.com>
2023-06-15 23:54:55 +0200

    Duplicates filter with test provided

---
+++ src/main/java/pl/edu/agh/hiputs/partition/mapper/filter/DuplicatesFilter.java
@@ -0,0 +1,145 @@
+package pl.edu.agh.hiputs.partition.mapper.filter;
+
+import de.topobyte.osm4j.core.model.iface.OsmEntity;
+import de.topobyte.osm4j.core.model.iface.OsmNode;
+import de.topobyte.osm4j.core.model.iface.OsmTag;
+import de.topobyte.osm4j.core.model.iface.OsmWay;
+import de.topobyte.osm4j.core.model.impl.Node;
+import de.topobyte.osm4j.core.model.impl.Tag;
+import de.topobyte.osm4j.core.model.impl.Way;
+import de.topobyte.osm4j.core.model.util.OsmModelUtil;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+import pl.edu.agh.hiputs.partition.osm.OsmGraph;
+
+@Service
+@Order(4)
+public class DuplicatesFilter implements Filter{
+
+  @Override
+  public OsmGraph filter(OsmGraph osmGraph) {
+    Collection<OsmNode> processingNodes = collectOrMergeNodesByLocation(osmGraph.getNodes());
+    Collection<OsmWay> processingWays = collectOrMergeWaysByLocation(osmGraph.getWays());
+
+    processingNodes = collectOrMergeNodesById(processingNodes);
+    processingWays = collectOrDeleteWaysById(processingWays);
+
+    // count nodes occurrences
+    Map<Long, Long> nodeId2Occurrences = processingWays.stream()
+        .flatMap(osmWay -> Arrays.stream(OsmModelUtil.nodesAsList(osmWay).toArray()).boxed())
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    // removing all single nodes (without ways)
+    processingNodes = processingNodes.stream()
+        .filter(osmNode -> nodeId2Occurrences.containsKey(osmNode.getId()))
+        .collect(Collectors.toList());
+
+    return new OsmGraph(processingNodes.stream().toList(), processingWays.stream().toList());
+  }
+
+  private Collection<OsmNode> collectOrMergeNodesByLocation(Collection<OsmNode> nodes) {
+    Map<NodeLocationWrapper, OsmNode> nodesByLocationAggregator = new HashMap<>();
+
+    nodes.forEach(nextNode -> {
+      NodeLocationWrapper nodeLocationKey = new NodeLocationWrapper(nextNode.getLongitude(), nextNode.getLatitude());
+      if (nodesByLocationAggregator.containsKey(nodeLocationKey)) {
+        // conflict => merge nodes and their contents with age priority for older ones
+        nodesByLocationAggregator.put(nodeLocationKey, mergeNodes(nodesByLocationAggregator.get(nodeLocationKey), nextNode));
+      } else {
+        nodesByLocationAggregator.put(nodeLocationKey, nextNode);
+      }
+    });
+
+    return nodesByLocationAggregator.values();
+  }
+
+  private Collection<OsmWay> collectOrMergeWaysByLocation(Collection<OsmWay> ways) {
+    Map<List<Long>, OsmWay> waysByNodesAggregator = new HashMap<>();
+
+    ways.forEach(nextWay -> {
+      List<Long> nextWayNodes = Arrays.stream(OsmModelUtil.nodesAsList(nextWay).toArray()).boxed().toList();
+      if (waysByNodesAggregator.containsKey(nextWayNodes)) {
+        // conflict => merge ways and their contents with age priority for older ones
+        waysByNodesAggregator.put(nextWayNodes, mergeWays(waysByNodesAggregator.get(nextWayNodes), nextWay));
+      } else {
+        waysByNodesAggregator.put(nextWayNodes, nextWay);
+      }
+    });
+
+    return waysByNodesAggregator.values();
+  }
+
+  private Collection<OsmNode> collectOrMergeNodesById(Collection<OsmNode> nodes) {
+    Map<Long, OsmNode> nodesByIdAggregator = new HashMap<>();
+
+    nodes.forEach(nextNode -> {
+      if (nodesByIdAggregator.containsKey(nextNode.getId())) {
+        // conflict => delete newer node with the same id
+        nodesByIdAggregator.put(nextNode.getId(), mergeNodes(nodesByIdAggregator.get(nextNode.getId()), nextNode));
+      } else {
+        nodesByIdAggregator.put(nextNode.getId(), nextNode);
+      }
+    });
+
+    return nodesByIdAggregator.values();
+  }
+
+  private Collection<OsmWay> collectOrDeleteWaysById(Collection<OsmWay> ways) {
+    Map<Long, OsmWay> waysByIdAggregator = new HashMap<>();
+
+    ways.forEach(nextWay -> {
+      if (waysByIdAggregator.containsKey(nextWay.getId())) {
+        // conflict => save older way
+        if (nextWay.getMetadata().getTimestamp() < waysByIdAggregator.get(nextWay.getId()).getMetadata().getTimestamp()) {
+          // proposed is older so replacing
+          waysByIdAggregator.put(nextWay.getId(), nextWay);
+        }
+      } else {
+        waysByIdAggregator.put(nextWay.getId(), nextWay);
+      }
+    });
+
+    return waysByIdAggregator.values();
+  }
+
+  private OsmNode mergeNodes(OsmNode first, OsmNode second) {
+    return first.getMetadata().getTimestamp() < second.getMetadata().getTimestamp() ?
+        new Node(first.getId(), first.getLongitude(), first.getLatitude(), mergeTags(first, second), first.getMetadata()) :
+        new Node(second.getId(), second.getLongitude(), second.getLatitude(), mergeTags(second, first), second.getMetadata());
+  }
+
+  private OsmWay mergeWays(OsmWay first, OsmWay second) {
+    return first.getMetadata().getTimestamp() < second.getMetadata().getTimestamp() ?
+        new Way(first.getId(), OsmModelUtil.nodesAsList(first), mergeTags(first, second), first.getMetadata()) :
+        new Way(second.getId(), OsmModelUtil.nodesAsList(second), mergeTags(second, first), second.getMetadata());
+  }
+
+  private <O extends OsmEntity> List<? extends OsmTag> mergeTags(O olderEntity, O newerEntity) {
+    Map<String, String> olderTags = OsmModelUtil.getTagsAsMap(olderEntity);
+    Map<String, String> newerTags = OsmModelUtil.getTagsAsMap(newerEntity);
+
+    newerTags.forEach((key, value) -> olderTags.merge(
+        key, value, (v1, v2) -> v1
+    ));
+
+    return olderTags.entrySet().stream()
+        .map(entry -> new Tag(entry.getKey(), entry.getValue()))
+        .toList();
+  }
+
+  @EqualsAndHashCode
+  @RequiredArgsConstructor
+  private static class NodeLocationWrapper {
+    private final double longitude;
+    private final double latitude;
+  }
+}
---
+++ src/test/java/pl/edu/agh/hiputs/partition/mapper/filter/DuplicatesFilterTest.java
@@ -0,0 +1,304 @@
+package pl.edu.agh.hiputs.partition.mapper.filter;
+
+import com.slimjars.dist.gnu.trove.list.array.TLongArrayList;
+import de.topobyte.osm4j.core.model.iface.OsmTag;
+import de.topobyte.osm4j.core.model.impl.Metadata;
+import de.topobyte.osm4j.core.model.impl.Node;
+import de.topobyte.osm4j.core.model.impl.Tag;
+import de.topobyte.osm4j.core.model.impl.Way;
+import de.topobyte.osm4j.core.model.util.OsmModelUtil;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.edu.agh.hiputs.partition.osm.OsmGraph;
+
+@ExtendWith(MockitoExtension.class)
+public class DuplicatesFilterTest {
+  private final DuplicatesFilter filter = new DuplicatesFilter();
+
+  @Test
+  public void mergeNodesByLocationWithUnCommonTags() {
+    // given
+    OsmTag osmTag1 = new Tag("highway", "primary");
+    OsmTag osmTag2 = new Tag("name", "Santa road");
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(osmTag1), metadata1),
+            new Node(2L, 1, 1, List.of(osmTag2), metadata2),
+            new Node(3L, 2, 2, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class)),
+            new Way(20L, new TLongArrayList(new long[]{2L, 3L}), List.of(), Mockito.mock(Metadata.class))
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(1L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(2L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(2, returned.getNodes().size());
+    returned.getNodes().stream()
+        .filter(osmNode -> osmNode.getId() == 1L)
+        .findAny()
+        .ifPresent(osmNode -> {
+          Assertions.assertEquals(2, osmNode.getNumberOfTags());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag1.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag1.getKey()), osmTag1.getValue());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag2.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag2.getKey()), osmTag2.getValue());
+        });
+  }
+
+  @Test
+  public void mergeNodesByLocationWithCommonTags() {
+    // given
+    OsmTag osmTag1 = new Tag("highway", "primary");
+    OsmTag osmTag2 = new Tag("name", "Santa road");
+    OsmTag osmTag3 = new Tag("highway", "secondary");
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(osmTag1), metadata1),
+            new Node(2L, 1, 1, List.of(osmTag2, osmTag3), metadata2),
+            new Node(3L, 2, 2, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class)),
+            new Way(20L, new TLongArrayList(new long[]{2L, 3L}), List.of(), Mockito.mock(Metadata.class))
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(2L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(1L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(2, returned.getNodes().size());
+    returned.getNodes().stream()
+        .filter(osmNode -> osmNode.getId() == 1L)
+        .findAny()
+        .ifPresent(osmNode -> {
+          Assertions.assertEquals(2, osmNode.getNumberOfTags());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag1.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag1.getKey()), osmTag3.getValue());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag2.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag2.getKey()), osmTag2.getValue());
+        });
+  }
+
+  @Test
+  public void mergeWaysByLocationWithUnCommonTags() {
+    // given
+    OsmTag osmTag1 = new Tag("highway", "primary");
+    OsmTag osmTag2 = new Tag("name", "Santa road");
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(), Mockito.mock(Metadata.class)),
+            new Node(2L, 2, 2, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 2L}), List.of(osmTag1), metadata1),
+            new Way(20L, new TLongArrayList(new long[]{1L, 2L}), List.of(osmTag2), metadata2)
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(1L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(2L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(1, returned.getWays().size());
+    Assertions.assertEquals(2, returned.getWays().get(0).getNumberOfTags());
+    Assertions.assertTrue(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).containsKey(osmTag1.getKey()));
+    Assertions.assertEquals(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).get(osmTag1.getKey()), osmTag1.getValue());
+    Assertions.assertTrue(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).containsKey(osmTag2.getKey()));
+    Assertions.assertEquals(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).get(osmTag2.getKey()), osmTag2.getValue());
+  }
+
+  @Test
+  public void mergeWaysByLocationWithCommonTags() {
+    // given
+    OsmTag osmTag1 = new Tag("highway", "primary");
+    OsmTag osmTag2 = new Tag("name", "Santa road");
+    OsmTag osmTag3 = new Tag("highway", "secondary");
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(), Mockito.mock(Metadata.class)),
+            new Node(2L, 2, 2, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 2L}), List.of(osmTag1), metadata1),
+            new Way(20L, new TLongArrayList(new long[]{1L, 2L}), List.of(osmTag2, osmTag3), metadata2)
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(2L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(1L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(1, returned.getWays().size());
+    Assertions.assertEquals(2, returned.getWays().get(0).getNumberOfTags());
+    Assertions.assertTrue(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).containsKey(osmTag1.getKey()));
+    Assertions.assertEquals(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).get(osmTag1.getKey()), osmTag3.getValue());
+    Assertions.assertTrue(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).containsKey(osmTag2.getKey()));
+    Assertions.assertEquals(OsmModelUtil.getTagsAsMap(returned.getWays().get(0)).get(osmTag2.getKey()), osmTag2.getValue());
+  }
+
+  @Test
+  public void mergeNodesByIdWithUnCommonTags() {
+    // given
+    OsmTag osmTag1 = new Tag("highway", "primary");
+    OsmTag osmTag2 = new Tag("name", "Santa road");
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(osmTag1), metadata1),
+            new Node(1L, 2, 2, List.of(osmTag2), metadata2),
+            new Node(3L, 3, 3, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class)),
+            new Way(20L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class))
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(1L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(2L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(2, returned.getNodes().size());
+    returned.getNodes().stream()
+        .filter(osmNode -> osmNode.getId() == 1L)
+        .findAny()
+        .ifPresent(osmNode -> {
+          Assertions.assertEquals(2, osmNode.getNumberOfTags());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag1.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag1.getKey()), osmTag1.getValue());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag2.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag2.getKey()), osmTag2.getValue());
+        });
+  }
+
+  @Test
+  public void mergeNodesByIdWithCommonTags() {
+    // given
+    OsmTag osmTag1 = new Tag("highway", "primary");
+    OsmTag osmTag2 = new Tag("name", "Santa road");
+    OsmTag osmTag3 = new Tag("highway", "secondary");
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(osmTag1), metadata1),
+            new Node(1L, 2, 2, List.of(osmTag2, osmTag3), metadata2),
+            new Node(3L, 3, 3, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class)),
+            new Way(20L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class))
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(2L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(1L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(2, returned.getNodes().size());
+    returned.getNodes().stream()
+        .filter(osmNode -> osmNode.getId() == 1L)
+        .findAny()
+        .ifPresent(osmNode -> {
+          Assertions.assertEquals(2, osmNode.getNumberOfTags());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag1.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag1.getKey()), osmTag3.getValue());
+          Assertions.assertTrue(OsmModelUtil.getTagsAsMap(osmNode).containsKey(osmTag2.getKey()));
+          Assertions.assertEquals(OsmModelUtil.getTagsAsMap(osmNode).get(osmTag2.getKey()), osmTag2.getValue());
+        });
+  }
+
+  @Test
+  public void deleteWaysByIdWithoutDeletingNodes() {
+    // given
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(), Mockito.mock(Metadata.class)),
+            new Node(2L, 2, 2, List.of(), Mockito.mock(Metadata.class)),
+            new Node(3L, 3, 3, List.of(), Mockito.mock(Metadata.class)),
+            new Node(4L, 4, 4, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 2L}), List.of(), metadata1),
+            new Way(10L, new TLongArrayList(new long[]{3L, 4L}), List.of(), metadata2),
+            new Way(30L, new TLongArrayList(new long[]{1L, 3L}), List.of(), Mockito.mock(Metadata.class)),
+            new Way(40L, new TLongArrayList(new long[]{2L, 4L}), List.of(), Mockito.mock(Metadata.class))
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(1L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(2L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(4, returned.getNodes().size());
+    Assertions.assertEquals(3, returned.getWays().size());
+    returned.getWays().stream()
+        .filter(osmWay -> osmWay.getId() == 10L)
+        .findAny()
+        .ifPresent(foundWay -> {
+          Assertions.assertTrue(OsmModelUtil.nodesAsList(foundWay).contains(1L));
+          Assertions.assertTrue(OsmModelUtil.nodesAsList(foundWay).contains(2L));
+        });
+  }
+
+  @Test
+  public void deleteWaysByIdWithDeletingNodes() {
+    // given
+    Metadata metadata1 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    Metadata metadata2 = Mockito.mock(Metadata.class, Mockito.RETURNS_DEEP_STUBS);
+    OsmGraph osmGraph = new OsmGraph(
+        List.of(
+            new Node(1L, 1, 1, List.of(), Mockito.mock(Metadata.class)),
+            new Node(2L, 2, 2, List.of(), Mockito.mock(Metadata.class)),
+            new Node(3L, 3, 3, List.of(), Mockito.mock(Metadata.class)),
+            new Node(4L, 4, 4, List.of(), Mockito.mock(Metadata.class))
+        ),
+        List.of(
+            new Way(10L, new TLongArrayList(new long[]{1L, 2L}), List.of(), metadata1),
+            new Way(10L, new TLongArrayList(new long[]{3L, 4L}), List.of(), metadata2)
+        ));
+
+    // when
+    Mockito.when(metadata1.getTimestamp()).thenReturn(1L);
+    Mockito.when(metadata2.getTimestamp()).thenReturn(2L);
+    OsmGraph returned = filter.filter(osmGraph);
+
+    // then
+    Assertions.assertEquals(2, returned.getNodes().size());
+    Assertions.assertEquals(1, returned.getWays().size());
+    returned.getWays().stream()
+        .filter(osmWay -> osmWay.getId() == 10L)
+        .findAny()
+        .ifPresent(foundWay -> {
+          Assertions.assertTrue(OsmModelUtil.nodesAsList(foundWay).contains(1L));
+          Assertions.assertTrue(OsmModelUtil.nodesAsList(foundWay).contains(2L));
+        });
+  }
+}
